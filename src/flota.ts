2b9b38b29a93567d82edcfeb9981bd915603a89e
pipQUasar <mftoba963@gmail.com>
2025-09-10 18:27:09 -0300

    Nueva_UML_y_planteamiento_de_Garage

---
+++ src/flota.ts
@@ -0,0 +1,25 @@
+export default class Flota{
+    private flota: Map<string,string>;
+
+    constructor(){
+        this.flota = new Map<string,string>();
+    }
+
+    public agregarVehiculo(categoria: string, disponibilidad: string): void{
+        this.flota.set(categoria, disponibilidad);
+    }
+
+    public obtenerVehiculo(categoria: string): string | undefined{
+        return this.flota.get(categoria);
+    }
+
+    public cambiarDisponibilidad(categoria: string, disponibilidad: string){
+        this.flota.set(categoria, disponibilidad); 'disponibilidad = Vehiculo.getDisponibilidad()'
+    }
+
+    public mostrarFlota(): void{
+        this.flota.forEach((disponibilidad, categoria) => {
+            console.log(`Categoria: ${categoria}, Disponibilidad: ${disponibilidad}`);
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-function main(){
+import Flota from "./flota";
 
+function main(){
+    const flotaVehiculos: Flota = new Flota();
+    flotaVehiculos.agregarVehiculo("Suv","Libre");
+    flotaVehiculos.agregarVehiculo("Compacto","Ocupado");
+    flotaVehiculos.agregarVehiculo("Sedan","En Mantenimiento");
+    flotaVehiculos.agregarVehiculo("Corsa","Ocupado padreee");
+
+    flotaVehiculos.mostrarFlota();
 }
 
 main();
