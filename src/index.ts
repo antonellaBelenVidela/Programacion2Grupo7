604c9dfb36b3b1813fccbc4d7539be09d1de5b25
antonellaBelenVidela <antonellabelenvidela@gmail.com>
2025-09-21 18:44:43 -0300

    realice la clase sedan con su calculo de tarifa y agregue libreria Moment.js para calcular tarifa.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
+import { Estado } from "./estado";
 import Flota from "./flota";
 import Sedan from "./vehiculos/sedan";
+import moment from "moment"; // IMPORLO LIBERIA DE MOMENT.JS
 
 function main(){
+
     const flotaVehiculos: Flota = new Flota();
-    const cochecito: Sedan = new Sedan("12345", "0km", 2500, "Alquilado", 1000, 1000);
-    flotaVehiculos.agregarVehiculo(cochecito.getPatente(), cochecito)
-    const cochecito2: Sedan = new Sedan("123456", "0km", 250066, "Disponible", 100066, 100660);
-    flotaVehiculos.agregarVehiculo(cochecito2.getPatente(),cochecito2);
-    
-    flotaVehiculos.obtenerDisponibilidad(cochecito.getPatente(), cochecito);
-    flotaVehiculos.obtenerDisponibilidad(cochecito.getPatente(), cochecito2);
+
+    const sedanAnto = new Sedan("AA222", Estado.ENALQUILER, 1000);
+    flotaVehiculos.agregarVehiculo(sedanAnto.getPatente(), sedanAnto);
+
+    // aca poner patente, fecha de inicio, fecha de fin ---> PUSE LIBRERIA moment.js QAUE EL PROFE RECOMENTDO EN UNA CLASE
+    const disponible = flotaVehiculos.obtenerDisponibilidad(sedanAnto.getPatente(), moment("2025-09-21", "YYYY-MM-DD").toDate(),moment("2025-09-22", "YYYY-MM-DD").toDate());
+
+    // miuestro datos para ver si funca
+    console.log(`El vehículo con patente: ${sedanAnto.getPatente()} está disponible? ${(disponible)}`);
+
+    console.log(sedanAnto.mostrarInfo());
+    console.log(`---> dando un total de $ ${sedanAnto.calcularTarifa(10,2)} por el alquiler del Sedan`)
 }
 
 main();
