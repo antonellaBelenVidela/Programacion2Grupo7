558ca13ad8635de7207b802b0525cf38288304fb
antonellaBelenVidela <antonellabelenvidela@gmail.com>
2025-10-15 22:45:35 -0300

    Agrego test unitarios de la clase Compacto.

--- tests/compacto.test.ts
+++ tests/compacto.test.ts
@@ -22,13 +22,44 @@ describe('Test Unitario de la Clase Compacto', () => {
     });
 
     // Hago prueba como si no tuviera kilómetros extra
-    test('Debería calcular el pago solo con la TARIFA DIARIA si es que NO SE EXCEDEN los 100km por día.', () => {
-        const kilometrosPorDia = [50, 80, 100]; // hago prueba con vector de km x 3 dias SI EXCEDERME DE 100km. 
+    test('Deberia calcular el PAGO solo con la TARIFA DIARIA si es que NO SE EXCEDEN los 100km por día.', () => {
+        const kilometrosPorDiaArray = [50, 80, 100]; // hago prueba con vector de km x 3 dias SI EXCEDERME DE 100km. 
         // Hago que el mock devuelva el array
-        mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue(kilometrosPorDia);
+        mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue(kilometrosPorDiaArray);
 
         const pagoEsperado = 30 * 3; // deberia dar 90
         expect(compacto.calcularPago(mockGestionKilometraje)).toBe(pagoEsperado);
     });
+
+    test('Deberia calcular el PAGO con EL CARGO EXTRA si es que un día se excede el límite (100km)', () => {
+    const kilometrosPorDiaArray = [100, 120, 90];
+    mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue(kilometrosPorDiaArray);
+
+    // D1: 30 - D2: 30 + (20 * 0.15) = 33 - D3: 30 ----> total: 93
+    const pagoEsperado = 30 + 33 + 30; // 93
+    expect(compacto.calcularPago(mockGestionKilometraje)).toBeCloseTo(pagoEsperado);
+  });
+
+   test('Deberia calcular de forma CORRECTA si TODOS LOS DÍAS se EXCEDE DEL LÍMITE de km diarios (100km)', () => {
+    const kilometrosPorDiaArray = [150, 200];
+    mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue(kilometrosPorDiaArray); // CUANDO S LLAME A ESTE METODO DEL MOCK va a retornar ese valor
+
+    // D1: 30 + (50 * 0.15) = 37.5 - D2: 30 + (100 * 0.15) = 45
+    const pagoEsperado = 37.5 + 45; // 82.5
+    expect(compacto.calcularPago(mockGestionKilometraje)).toBeCloseTo(pagoEsperado);
+  });
+
+  test('Deberia devolver 0 si NO hay kilómetros registrados (SI EL ARRAY ESTÁ VACIO)', () => {
+    mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue([]);
+
+    expect(compacto.calcularPago(mockGestionKilometraje)).toBe(0);
+  });
+
+  test('Deberia LLAMAR al método getKilometrosRecorridosPordias de GestionKilometraje', () => {
+    mockGestionKilometraje.getKilometrosRecorridosPordias.mockReturnValue([90]);
+    compacto.calcularPago(mockGestionKilometraje);
+    expect(mockGestionKilometraje.getKilometrosRecorridosPordias).toHaveBeenCalled();
+  });
+
 
 });
