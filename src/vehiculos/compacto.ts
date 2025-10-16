e8fc97b3a3d90b0ace84a228cd2bdf54b3584c1f
antonellaBelenVidela <antonellabelenvidela@gmail.com>
2025-10-15 23:07:27 -0300

    Agrego Documentacion (clase, parametros, retornos,ejemplo, y extension) de la clase Compacto.

--- src/vehiculos/compacto.ts
+++ src/vehiculos/compacto.ts
@@ -1,19 +1,61 @@
 import Kilometraje from "../gestiones/gestionKilometraje";
 import Vehiculo from "./vehiculo";
 
+/**
+ * Clase que representa un vehículo del tipo Compacto.
+ * 
+ * la clase Compacto Extiende la clase {@link Vehiculo} e implementa una lógica de cálculo de pago basada en una tarifa base diaria de $30 y un cargo extra ($0.15) por kilómetro excedido (una vez pasas los 100km del dia).
+ * 
+ * @class Compacto
+ * @extends Vehiculo
+ */
+
 export default class Compacto extends Vehiculo {
 
+  /**
+   * Tarifa base diaria del vehículo Compacto.
+   * @type {number}
+   */
   private tarifaBase: number = 30;
-  private cargoExtraPorKm: number = 0.15;
-  private limiteKmPorDia: number = 100;
 
+  /** 
+   * Cargo Extra por cada km excedido del límite diario.
+   * @type {number}
+   */
+  private cargoExtraPorKm: number = 0.15;
+
+  /**
+   * Limite de kms permitidos por día (sin un cargo adicional).
+   * @type {number}
+   */
+    private limiteKmPorDia: number = 100;
+
+  /**
+   * Crea una instancia de la clase Compacto.
+   */
   constructor() {
     super();
   }
+
   /**
-   * 
-   * @param kilometraje recibe la clase kilometraje 
+
    * @returns devuelve la cantidad que debe en cuestion 
+   */
+
+  /**
+   * El metodo calcularPago calcula el pago total que debe realizar el cliente según los kilómetros recorridos por día.
+   * 
+   * -> Tiene en cuenta una tarifa base diaria.
+   * -> Si se exceden los 100km por día, cobra un cargo adicional por cada km extra.
+   * 
+   * @param {Kilometraje} kilometraje -> Recibe e Instancia la clase Kilometraje que contiene los kilómetros recorridos por día.
+   * @returns {number} -> Retorna el monto total a pagar por el alquiler del vehículo.
+   * 
+   * @example
+   * const km = new Kilometraje([144, 76, 112]);
+   * const auto = new Compacto();
+   * const total = auto.calcularPago(km);
+   * console.log(total); // Devuelve el pago total con cargos extra incluidos
    */
 
   public calcularPago(kilometraje: Kilometraje): number {
